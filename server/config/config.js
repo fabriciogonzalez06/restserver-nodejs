961494d78db0c9c55220e59bb03d5a39c714c0bb
Fabricio <angelfabriciogonzalez@gmail.com>
2019-10-26 15:42:44 -0600

    variable de entorno heroku

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://fabricio:o69rZ4rdeKauXX5f@cluster0-ausnr.mongodb.net/cafe';
+    //variable de entorno creada en heroku con la conexion a la base de datos mongo heroku config:set nombre=""
+    urlDB = process.env.MONGODB_URI;
 }
 
 process.env.URLDB = urlDB;
